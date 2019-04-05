48bb782ebbaaedf51c280c71e5b6265998ecfcd1
liuhuishan <liuhuishan@deepexi.com>
2019-04-05 10:21:40 +0800

    1.添加redission依赖 2.添加logback配置 3.示例代码加入redission示例

--- src/main/java/com/deepexi/demo/controller/HelloController.java
+++ src/main/java/com/deepexi/demo/controller/HelloController.java
@@ -2,7 +2,6 @@ package com.deepexi.demo.controller;
 
 import com.deepexi.demo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
--- src/main/java/com/deepexi/demo/service/impl/UserServiceImpl.java
+++ src/main/java/com/deepexi/demo/service/impl/UserServiceImpl.java
@@ -1,8 +1,16 @@
 package com.deepexi.demo.service.impl;
 
+import cn.hutool.core.util.StrUtil;
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 import com.deepexi.demo.entity.User;
 import com.deepexi.demo.mapper.UserMapper;
 import com.deepexi.demo.service.UserService;
+import org.redisson.api.RBucket;
+import org.redisson.api.RedissonClient;
+import org.redisson.client.RedisClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.cache.annotation.EnableCaching;
@@ -21,8 +29,12 @@ import java.util.List;
 @Service
 public class UserServiceImpl implements UserService {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(UserServiceImpl.class);
+
     @Autowired
     private UserMapper userMapper;
+    @Autowired
+    private RedissonClient redissonClient;
 
 
     @Transactional
@@ -34,7 +46,18 @@ public class UserServiceImpl implements UserService {
     @Override
     public User findById(Long id) {
         User user = new User();
-        user.setId(Math.toIntExact(id));
-        return userMapper.selectOne(user);
+        RBucket<String> keyBucket = redissonClient.getBucket("USER_ID_"+id);
+        String value = keyBucket.get();
+        LOGGER.info("根据key获取值，key不存在也会进行映射，判断是否有缓存 key:{}--value:{}","USER_ID_"+id,value);
+        if(StrUtil.isEmpty(value)){
+            LOGGER.info("value:{} 为空",value);
+            user.setId(Math.toIntExact(id));
+            user = userMapper.selectOne(user);
+            keyBucket.set(JSON.toJSONString(user));
+        }else {
+            LOGGER.info("value:{} 不为空",value);
+            user = JSONObject.parseObject(value,User.class);
+        }
+        return user;
     }
 }
